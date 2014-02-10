d3bd2e604f44463bb4bffb8936f673a962a36f07
tamubun <sutebubon@gmail.com>
2014-02-10 23:55:22 +0900

    z座標が画面上を向くようにした。

--- computer/moon/moon.js
+++ computer/moon/moon.js
@@ -12,7 +12,7 @@ var celestial; // 天球
 
 function newSettings() {
   latitude = $('#latitude').val() / 180.0 * Math.PI;
-  celestial.quaternion.setFromAxisAngle(e2, Math.PI/2 - latitude);
+  celestial.quaternion.setFromAxisAngle(e1, latitude);
 }
 
 function init() {
@@ -21,7 +21,7 @@ function init() {
 
   camera = new THREE.PerspectiveCamera(
     45, arena.innerWidth() / arena.innerHeight(), 1, 2000);
-  camera.position.set(0, -700, 350);
+  camera.position.set(750, 50, 350); // x: 画面手前向き, y: 右向き, z: 上向き
   camera.up = e3.clone();
   camera.lookAt(scene.position);
   controls = new THREE.TrackballControls(camera, arena[0]);
@@ -45,7 +45,7 @@ function init() {
   var polaris = new THREE.Mesh(        // 天の北極
     new THREE.SphereGeometry(5), 
     new THREE.MeshLambertMaterial({ color: "black"}));
-  polaris.position.z = radius;
+  polaris.position.y = radius;
   celestial.add(polaris);
 
   var material = new THREE.MeshLambertMaterial();
@@ -53,19 +53,22 @@ function init() {
   var sun_trajectory = new THREE.Mesh( // 春分、秋分
     new THREE.TorusGeometry(radius,1,3,50), 
     material);
+  sun_trajectory.quaternion.setFromAxisAngle(e1, Math.PI/2);
   celestial.add(sun_trajectory);
 
   sun_trajectory = new THREE.Mesh(     // 夏至
     new THREE.TorusGeometry(radius * Math.cos(23.4/180*Math.PI),1,3,50),
     material);
+  sun_trajectory.quaternion.setFromAxisAngle(e1, Math.PI/2);
+  sun_trajectory.position.y = radius * Math.sin(23.4/180*Math.PI)
   celestial.add(sun_trajectory);
-  sun_trajectory.position.z = radius * Math.sin(23.4/180*Math.PI)
 
   sun_trajectory = new THREE.Mesh(     // 冬至
     new THREE.TorusGeometry(radius * Math.cos(23.4/180*Math.PI),1,3,50),
     material);
+  sun_trajectory.quaternion.setFromAxisAngle(e1, Math.PI/2);
+  sun_trajectory.position.y = -radius * Math.sin(23.4/180*Math.PI)
   celestial.add(sun_trajectory);
-  sun_trajectory.position.z = -radius * Math.sin(23.4/180*Math.PI)
 
   var ground = new THREE.Mesh(
     new THREE.CubeGeometry(600, 600,radius * 1.1),
